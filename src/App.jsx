4c7b13717b44ab5b44f27cb5e6764585ca4a4e97
Sathvik1109 <saathvikdugyala09@gmail.com>
2026-03-25 21:51:28 -0500

    update commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import Header from "./Header";
 import Main from "./Body";
+import { useEffect, useReducer } from "react";
+
+const reducer = (state, action) => {
+  return state;
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    const getFetch = async () => {
+      try {
+        const res = await fetch("http://localhost:9000/questions");
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getFetch();
+  }, []);
   return (
     <div className="app">
       <Header />
