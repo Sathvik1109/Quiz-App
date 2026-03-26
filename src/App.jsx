b9ee376d6ca50b3e585c1904ff74e58ce23c8e73
Sathvik1109 <saathvikdugyala09@gmail.com>
2026-03-26 09:39:33 -0500

    update commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import Main from "./Body";
 import { useEffect, useReducer } from "react";
 
 const reducer = (state, action) => {
-  return state;
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload.data, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action is unknown");
+  }
+};
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished' states
+  status: "loading",
 };
 
 function App() {
@@ -13,13 +26,18 @@ function App() {
       try {
         const res = await fetch("http://localhost:9000/questions");
         const data = await res.json();
-        console.log(data);
+        dispatch({ type: "dataReceived", payload: { data: data } });
       } catch (err) {
-        console.error(err);
+        dispatch({ type: "dataFailed" });
       }
     };
     getFetch();
   }, []);
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
   return (
     <div className="app">
       <Header />
